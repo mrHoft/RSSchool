39a5e09b5482b771a2bf00a1eff3764c342ca3eb
mrHoft <ru.hoft@gmail.com>
2023-11-01 15:35:04 +0300

    Seach component rebuilded to class

--- packages/react-w1/src/components/search/Search.tsx
+++ packages/react-w1/src/components/search/Search.tsx
@@ -3,46 +3,73 @@ import styles from './.module.css';
 import Store from '../../store/Store';
 
 const store = new Store();
-let lastSearch = store.get('lastSearch');
-if (typeof lastSearch !== 'string') lastSearch = '';
 
 interface SearchProps {
-  mode?: 'static' | 'dynamic';
   callback: (query: string) => void;
 }
 
-export const Search = ({ callback, mode = 'static' }: SearchProps) => {
-  const [value, setValue] = React.useState<string>(lastSearch as string);
+interface SearchState {
+  callback: (query: string) => void;
+  value: string;
+}
 
-  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
+export class Search extends React.Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+    this.state = { ...props, value: '' };
+  }
+
+  componentDidMount(): void {
+    let lastSearch = store.get('lastSearch');
+    if (typeof lastSearch !== 'string') lastSearch = '';
+    else
+      this.setState((props) => {
+        return { ...props, value: lastSearch as string };
+      });
+  }
+
+  private changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState((props) => {
+      return { ...props, value: event.target.value };
+    });
   };
 
-  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+  private submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const form = new FormData(event.target as HTMLFormElement);
+    const value = String(form.get('request'));
     if (value) {
       store.set('lastSearch', value);
     }
-    callback(value);
+    this.state.callback(value);
   };
 
-  return (
-    <form onSubmit={submitHandler} className={styles.search__form}>
-      <div className={mode === 'static' ? styles.search__field_static : styles.search__field}>
-        <input
-          type="text"
-          name="request"
-          placeholder="Search"
-          autoFocus={true}
-          autoComplete="off"
-          className={mode === 'static' ? styles.search__input_static : styles.search__input}
-          value={value}
-          onChange={changeHandler}
-        />
-        <div className={mode === 'static' ? styles.search__clear_static : styles.search__clear} onClick={() => setValue('')}></div>
-        {mode !== 'static' ? <input type="submit" value="" className={styles.search__submit} /> : ''}
-      </div>
-      <button className={styles.search__button}>Start search</button>
-    </form>
-  );
-};
+  render() {
+    return (
+      <>
+        <form onSubmit={this.submitHandler} className={styles.search__form}>
+          <div className={styles.search__field_static}>
+            <input
+              type="text"
+              name="request"
+              placeholder="Search"
+              autoFocus={true}
+              autoComplete="off"
+              className={styles.search__input_static}
+              value={this.state.value}
+              onChange={this.changeHandler}
+            />
+            <div
+              className={styles.search__clear_static}
+              onClick={() =>
+                this.setState((props) => {
+                  return { ...props, value: '' };
+                })
+              }></div>
+          </div>
+          <button className={styles.search__button}>Start search</button>
+        </form>
+      </>
+    );
+  }
+}
